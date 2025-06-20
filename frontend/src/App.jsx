58c5e866f091d0df4fffe7388b6836dfdae1b06d
Paras Kalyan <paraskalyan20@gmail.com>
2025-06-20 23:59:53 +0530

    UPdate

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,22 @@
 import { useState } from 'react'
 import './App.css'
 import { Button } from './components/ui/button'
-
+import { Routes, Route } from 'react-router'
+import Home from './Pages/Home'
+import Login from './Pages/Login'
+import Signup from './Pages/Signup'
+import Navbar from './components/Navbar'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Button>HELLO WORLD</Button>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<Signup />} />
+      </Routes>
     </>
   )
 }
