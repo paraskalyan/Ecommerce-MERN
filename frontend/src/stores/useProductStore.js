e0d76eebfb0cad078fd7c2692e1394f25ee687d4
Paras Kalyan <paraskalyan20@gmail.com>
2025-06-25 18:15:35 +0530

    Created get all products function in store

--- frontend/src/stores/useProductStore.js
+++ frontend/src/stores/useProductStore.js
@@ -1,6 +1,7 @@
 import { create } from "zustand";
 import { toast } from "react-hot-toast";
 import axiosInstance from "../lib/axios";
+import { getAllProducts } from "../../../backend/controllers/product.controller";
 const useProductStore = create((set) => ({
   products: [],
   setProducts: (products) => set({ products }),
@@ -17,6 +18,19 @@ const useProductStore = create((set) => ({
     } catch (error) {
       console.error("Error creating product:", error);
       toast.error("Failed to create product. Please try again.");
+    } finally {
+      set({ loading: false });
+    }
+  },
+
+  getAllProducts: async () => {
+    set({ loading: true });
+    try {
+      const res = await axiosInstance.get("/products");
+      set({ products: res.data.products });
+    } catch (error) {
+      console.error("Error fetching products:", error);
+      toast.error("Failed to fetch products. Please try again.");
     } finally {
       set({ loading: false });
     }
