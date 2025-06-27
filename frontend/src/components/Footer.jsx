e4c5e49660fe5d7829483b2964aa4670d11aea1c
Paras Kalyan <paraskalyan20@gmail.com>
2025-06-28 00:11:38 +0530

    footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -4,15 +4,15 @@ import { Button } from './ui/button'
 
 const Footer = () => {
     return (
-        <footer className='py-10 bg-black mt-15 flex items-center justify-center text-white'>
+        <footer className='py-10 bg-black mt-15 flex flex-col lg:flex-row items-center justify-center text-white'>
             <div className='flex-[2] flex items-center flex-col  '>
-                <h1 className='text-6xl font-semibold text-white'>ARE YOU INTERESTED?</h1>
-                <Button className='mt-10'>Shop now <ArrowRight /></Button>
+                <h1 className='lg:text-6xl text-3xl  font-semibold text-white'>ARE YOU INTERESTED?</h1>
+                <Button className='lg:mt-10 mt-2'>Shop now <ArrowRight /></Button>
             </div>
 
-            <div className='flex-1 space-y-7'>
+            <div className='flex-1 space-y-7 mt-10 lg:mt-0'>
                 <img src='/logo-2.png' width={170} />
-                <div className='space-y-4'>
+                <div className='space-y-4 text-center'>
                     <h6 className='font-medium'>Shop</h6>
                     <h6 className='font-medium'>About</h6>
                     <h6 className='font-medium'>Blog</h6>
