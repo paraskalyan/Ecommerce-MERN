c65141ad5de40f9a32128fbb23f04df6632578bb
Paras Kalyan <paraskalyan20@gmail.com>
2025-06-28 18:19:45 +0530

    updates

--- backend/index.js
+++ backend/index.js
@@ -19,12 +19,12 @@ const PORT = process.env.PORT || 3000;
 connectDB();
 
 app.use(cookieParser());
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: "http://localhost:3000",
+//     credentials: true,
+//   })
+// );
 app.use(express.json({ limit: "10mb" }));
 
 app.use("/api/auth", authRoutes);
